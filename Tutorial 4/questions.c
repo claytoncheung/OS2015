72daf22489e0dc73698db520337935c4f70a9b3a
AsmySarasan <asmy.sarasan@uoit.net>
2015-10-13 23:43:01 -0400

    Update questions.c

--- Tutorial 4/questions.c
+++ Tutorial 4/questions.c
@@ -15,20 +15,20 @@
 void initialize_game(void)
 {
 	  // initialize each question struct and assign it to the questions array
-	strcpy(questions[0].category, categories[0]); strcpy(questions[0].question,""); strcpy(questions[0].answer,""); questions[0].value=200; questions[0].answered=false;
-	strcpy(questions[1].category, categories[0]); strcpy(questions[1].question, ""); strcpy(questions[1].answer, ""); questions[1].value=400; questions[1].answered=false;
-	strcpy(questions[2].category, categories[0]); strcpy(questions[2].question, ""); strcpy(questions[2].answer, ""); questions[2].value=600; questions[2].answered=false;
-	strcpy(questions[3].category, categories[0]); strcpy(questions[3].question, ""); strcpy(questions[3].answer, ""); questions[3].value=800; questions[3].answered=false;
+	strcpy(questions[0].category, categories[0]); strcpy(questions[0].question,"Name a loop type that starts with F: "); strcpy(questions[0].answer,"for loop"); questions[0].value=200; questions[0].answered=false;
+	strcpy(questions[1].category, categories[0]); strcpy(questions[1].question, "This language is an example for object oriented programming: "); strcpy(questions[1].answer, "Java"); questions[1].value=400; questions[1].answered=false;
+	strcpy(questions[2].category, categories[0]); strcpy(questions[2].question, "In the C programming language, what code is used to open a file: "); strcpy(questions[2].answer, "fopen"); questions[2].value=600; questions[2].answered=false;
+	strcpy(questions[3].category, categories[0]); strcpy(questions[3].question, "These are variable that store memory address information of other variables: "); strcpy(questions[3].answer, "pointers"); questions[3].value=800; questions[3].answered=false;
 	
-	strcpy(questions[4].category, categories[1]); strcpy(questions[4].question, ""); strcpy(questions[4].answer, ""); questions[4].value=200; questions[4].answered=false;
-	strcpy(questions[5].category, categories[1]); strcpy(questions[5].question, ""); strcpy(questions[5].answer, ""); questions[5].value=400; questions[5].answered=false;
-	strcpy(questions[6].category, categories[1]); strcpy(questions[6].question, ""); strcpy(questions[6].answer, ""); questions[6].value=600; questions[6].answered=false;
-	strcpy(questions[7].category, categories[1]); strcpy(questions[7].question, ""); strcpy(questions[7].answer, ""); questions[7].value=800; questions[7].answered=false;
+	strcpy(questions[4].category, categories[1]); strcpy(questions[4].question, "This method of analysis is used to find the Big O of Merge sort: "); strcpy(questions[4].answer, "recursive"); questions[4].value=200; questions[4].answered=false;
+	strcpy(questions[5].category, categories[1]); strcpy(questions[5].question, "This method is used to prove our algorithm is true (2 words): "); strcpy(questions[5].answer, "loop invariants"); questions[5].value=400; questions[5].answered=false;
+	strcpy(questions[6].category, categories[1]); strcpy(questions[6].question, "This is a search function that has a recursive algorithm where a = 1 but b = 2: "); strcpy(questions[6].answer, "binary search"); questions[6].value=600; questions[6].answered=false;
+	strcpy(questions[7].category, categories[1]); strcpy(questions[7].question, "The final step in checking the correctness of an algorithm is: "); strcpy(questions[7].answer, "termination"); questions[7].value=800; questions[7].answered=false;
 	
-	strcpy(questions[8].category, categories[2]); strcpy(questions[8].question, ""); strcpy(questions[8].answer, ""); questions[8].value=200; questions[8].answered=false;
-	strcpy(questions[9].category, categories[2]); strcpy(questions[9].question, ""); strcpy(questions[9].answer, ""); questions[9].value=400; questions[9].answered=false;
-	strcpy(questions[10].category, categories[2]); strcpy(questions[10].question, ""); strcpy(questions[10].answer, ""); questions[10].value=600; questions[10].answered=false;
-	strcpy(questions[11].category, categories[2]); strcpy(questions[11].question, ""); strcpy(questions[11].answer, ""); questions[11].value=800; questions[11].answered=false;	
+	strcpy(questions[8].category, categories[2]); strcpy(questions[8].question, "The word for unique entry used to access each table:"); strcpy(questions[8].answer, "key"); questions[8].value=200; questions[8].answered=false;
+	strcpy(questions[9].category, categories[2]); strcpy(questions[9].question, "This is the name of a key that can be used to access an outside table: "); strcpy(questions[9].answer, "foreign key"); questions[9].value=400; questions[9].answered=false;
+	strcpy(questions[10].category, categories[2]); strcpy(questions[10].question, "In formal relational model, a row is called : "); strcpy(questions[10].answer, "tuple"); questions[10].value=600; questions[10].answered=false;
+	strcpy(questions[11].category, categories[2]); strcpy(questions[11].question, "In formal relational model, a column is called : "); strcpy(questions[11].answer, "attribute"); questions[11].value=800; questions[11].answered=false;	
 }
 
 // Displays each of the remaining categories and question dollar values that have not been answered
