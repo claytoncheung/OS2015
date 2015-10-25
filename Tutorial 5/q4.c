0012f5d2bd69c0c59283381c63f3a2efb00e1d35
Clayton Cheung <clayton.cheung@uoit.net>
2015-10-24 23:39:10 -0400

    fixed q4

--- Tutorial 5/q4.c
+++ Tutorial 5/q4.c
@@ -1,39 +1,41 @@
-#define _BSD_SOURCE
 #include <stdlib.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
+#define NUM_STUDENTS 10
 
 int total_grade;
 pthread_mutex_t mutex;
 
 void *class_total(void *arg){
 
-	int grade = (intptr_t)arg;
-	usleep(10);
+	int *grade = (int *)arg;
 	pthread_mutex_lock (&mutex);
-	total_grade += grade;
+	total_grade += *grade;
 	pthread_mutex_unlock (&mutex);
 	pthread_exit(NULL);
 }
 
 int main(void){
 
-	pthread_t student_grades[10];
+	pthread_t student_grades[NUM_STUDENTS];
 	pthread_mutex_init(&mutex, NULL);
-	int  grade_in;
+	int  grade[NUM_STUDENTS];
+	int i=0;
 
-	printf("Please enter the student's grades:\n");
-
-	for(int i = 0;i<10;i++){
-		scanf("%d\n", &grade_in);
-		pthread_create(&student_grades[grade_in], NULL, class_total, (void *)(intptr_t)grade_in);
-	  	pthread_join(student_grades[grade_in], 0);
+	printf("Please enter the 10 student's grades :\n");
+	
+	for (i=0;i<NUM_STUDENTS;i++){
+	    scanf("%d", &grade[i]);
 	}
+
+	for(i = 0;i<NUM_STUDENTS;i++)
+		pthread_create(&student_grades[i], 0, class_total, (void *) &grade[i]);
+	
+	for (i=0;i<NUM_STUDENTS;i++)
+	  	pthread_join(student_grades[i], NULL);
 
 	printf("The total grade is: %d\n", total_grade);
 	pthread_exit(NULL);
-	return(0);	
+	return 0;	
 }
 
