868c6237c8b1a83ae9239848f35c32854d8aba52
Clayton Cheung <clayton.cheung@uoit.net>
2015-11-17 20:21:53 -0500

    Q1 completed

--- Tutorial 8/q1.c
+++ Tutorial 8/q1.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 typedef struct {
     char parent[256];
@@ -16,28 +16,33 @@ typedef struct {
 
 typedef struct proc_tree {
     proc process;
-    proc_tree* left = NULL;
-    proc_tree* right = NULL;
+    struct proc_tree* left;
+    struct proc_tree* right;
 } proc_tree_t;
-
-void dfs(struct proc_tree_t* tree, struct proc_tree_t* toFind);
-void add_node (struct proc_tree_t* tree, struct proc process);
 
 static bool added = false;
 static bool found = false;
 
+void dfs( proc_tree_t* tree,  proc_tree_t* toFind);
+void add_node ( proc_tree_t* tree,  proc_tree_t* toAdd);
+void printTree (proc_tree_t* tree);
+
+
+
 int main() {
-    char* line = NULL;
+    char* line = (char *) malloc(256*sizeof(char));
 	size_t len = 0;
-    proc_tree_t root = (proc_tree_t *) malloc(sizeof(proc_tree_t));
+    ssize_t read;
+    proc_tree_t *root = (proc_tree_t *) malloc(sizeof(proc_tree_t));
     root = NULL;
 
     //parent, name, priority, memory
     //7 lines
-    FILE *fp = fopen("process_tree.txt", "r");
-
+    FILE *fp = fopen("processes_tree.txt", "r");
     while ((read = getline(&line, &len, fp)) != -1) {
         proc_tree_t* node = (proc_tree_t *) malloc(sizeof(proc_tree_t));
+        node->left=NULL;
+        node->right=NULL;
 
         strcpy(node->process.parent, strtok(line, ", "));
         strcpy(node->process.name, strtok(NULL, ", "));
@@ -47,48 +52,82 @@ int main() {
         if(root != NULL) {
             found = false;
             added = false;
-            dfs(*root, *node);
+            dfs(root, node);
         }
-        if (!root)
-            memcpy(root, node, sizeof(proc_tree_t));
+        if (!root) {
+            root = node;
+        }
+    }
+    printTree(root);
+}
+
+void printTree(proc_tree_t* tree) {
+    printf("\nParent Name: %s, ", tree->process.name);
+    printf("Parent Priority: %d, ", tree->process.priority);
+    printf("Parent Memory: %d, ", tree->process.memory);
+    if (tree->left != NULL){
+        printf("Left Child Name: %s, ", tree->left->process.name);
+        if (tree->right != NULL){
+            printf("Right Child Name: %s", tree->right->process.name);
+            printTree(tree->right);
+        }
+        printTree(tree->left);
+        return;
+    }
+    if (tree->right != NULL){
+        printf("Right Child Name: %s", tree->right->process.name);
+        printTree(tree->right);
     }
 }
 
 void dfs (proc_tree_t* tree, proc_tree_t* toFind) {
-    if (found != true) {
+    proc_tree_t* node = (proc_tree_t *) malloc(sizeof(proc_tree_t));
+    node = NULL;
+
+    if (found == false) {
+
+        if (strcmp(toFind->process.parent, tree->process.name) == 0) {
+            found = true;
+            add_node(tree,toFind);
+            return;
+        }
+
         if (tree->left != NULL) {
-            if (toFind.process.parent == tree->left.process.parent){
-                tree->left = toFind;
+            if (strcmp(toFind->process.parent, tree->left->process.name) == 0) {
                 found = true;
-                add_node(tree->left, toFind)
+                node = tree->left;
+                add_node(node, toFind);
+                tree = toFind;
                 return;
             }
             dfs(tree->left, toFind);
         }
 
         if (tree->right != NULL) {
-            if (toFind.process.parent == tree->right.process.parent){
-                tree->right = toFind;
+            if (strcmp(toFind->process.parent, tree->right->process.name) == 0) {
                 found = true;
-                add_node(tree->right, toFind);
+                node = tree->right;
+                add_node(node, toFind);
+                tree = toFind;
                 return;
             }
-            dfs(tree->right);
+            dfs(tree->right, toFind);
         }
+
     }
 }
 
 void add_node (proc_tree_t* tree, proc_tree_t* toAdd) {
-    if (added != true) {
+    if (added == false) {
         if (tree->left == NULL) {
-            proc_tree_t* left = (proc_tree_t *) malloc(sizeof(proc_tree_t));
-            memcpy(left, toAdd, sizeof(proc_tree_t));
+            tree->left = toAdd;
             added = true;
+            return;
         }
         if (tree->right == NULL){
-            proc_tree_t* right = (proc_tree_t *) malloc(sizeof(proc_tree_t));
-            memcpy(right, toAdd, sizeof(proc_tree_t));
+            tree->right = toAdd;
             added = true;
+            return;
         }
     }
 }
