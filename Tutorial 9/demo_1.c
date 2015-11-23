104a1b8e9ccf567367c55b05d843f1e0b7263fee
Malek Mustapha-Abdullah <malek.mustaphaabdullah@uoit.net>
2015-11-22 23:27:48 -0500

    Update demo_1.c

--- Tutorial 9/demo_1.c
+++ Tutorial 9/demo_1.c
@@ -3,6 +3,8 @@
 int main (int argc, char *argv[])
 {
     printf("This statement will always print.\n");
+    
+    //The statement is conditional and will only be executed if _OPEN is defined
     #ifdef _OPENMP
     printf("This statement will only print when compiled with \"-fopenmp\"\n");
     #endif
