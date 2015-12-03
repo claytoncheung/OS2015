ac151fe0f938d879935f2e9f45064339d6fdf43b
Clayton Cheung <clayton.cheung@uoit.net>
2015-12-02 22:09:45 -0500

    q4

--- Tutorial 10/q4.c
+++ Tutorial 10/q4.c
@@ -5,21 +5,22 @@
 #include <mpi.h>
 
 #define MASTER 0
-#define DATA_SIZE 10000
-#define CHUNK_SIZE 100
-#define LOWER	100
-#define UPPER	999
+#define DATA_SIZE 1000
+#define CHUNK_SIZE 333
+#define LOWER	1
+#define UPPER	1000
+
 //	Determine if a number is a prime or not by counting the number of
 //	roots it has
 bool isPrime(int number) {
 	int rootCounter = 0;
 
-	for (int i = 0; i <= number; i++) {
+	for (int i = 2; i < number; i++) {
 		if (number%i == 0)
 			rootCounter++;
 	}
 
-	if (rootCounter < 3)
+	if (rootCounter == 0)
 		return true;
 	else
 		return false;
@@ -70,30 +71,32 @@ void master(int n_proc) {
 		if (data[i] < 1)
 			printf("ERROR, DATA ZERO AT %d\n", i);
 	}
-
-	for (int i = 0; i < DATA_SIZE; ++i){
-		if (results[i] != 0)
-			printf("%d ", i);
+	int i = 0;
+	while(results[i] != 0){
+		printf("%d, ", i);
+		i++;
 	}
 }
 //Given a range of numbers, return the prime numbers within that range
-void slave(int n_proc) {
+void slave(int proc_id) {
 	int chunk[CHUNK_SIZE] = {0};
 	int result[CHUNK_SIZE] = {0};
 	int n_recv = 0;
 	MPI_Status status;
 	int arrayCounter = 0;
+	int start;
 
 	MPI_Recv(chunk, CHUNK_SIZE, MPI_INT, MASTER, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 	n_recv = status.MPI_TAG;
+	if ((chunk[0]%2) == 0){
+		start = chunk[1];
+	}
+	if ((chunk[0]%2) != 0) {
+		start = chunk[0];
+	}
 
 	while (n_recv < CHUNK_SIZE) {
-		if (assignedNumbers[0]%2 == 0)
-			int start = assignedNumbers[0]+1;
-		if (assignedNumbers[0]%2 != 0)
-			int start = assignedNumbers[0];
-
-		for (int i = start; i < CHUNK_SIZE; i += 2) {
+		for (int i = start; i < (CHUNK_SIZE+start); i += 2) {
 			if (isPrime(i) == true) {
 					result[arrayCounter] = i;
 					arrayCounter++;
@@ -110,26 +113,12 @@ void slave(int n_proc) {
 int main(int argc, char* argv[]) {
 	int proc_id;		//Process Rank
 	int n_proc;			// Number of Processes
-	int slaveNums[2] = {0,0};
 
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
-
-
-	dividedRange = nearbyint((upperBound - lowerBound)/3);
-
-	int primes[SLAVE_NUM][dividedRange];
-
-	for (int i = 0; i < (SLAVE_NUM + 1); i ++) {
-		if (proc_id != MASTER) {
-			slaveNums[0] = (lowerBound + (i*dividedRange));
-			slaveNums[1] = (upperBound + (i*dividedRange));
-			*primes[i] = slave(s);
-		}
-	}
 
 	if (proc_id == MASTER)
 		master(n_proc);
